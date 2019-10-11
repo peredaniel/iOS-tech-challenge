05470c3888e07a2eccbdd68e4f1dcd54fef6b540
Pere Daniel Prieto <math.pedro.daniel.prieto@gmail.com>
2019-10-11 13:30:08 +0200

    Improved UI and behavior of track details screen.

--- ABA Music/View controllers & view models/Track details/TrackDetailsViewController.swift
+++ ABA Music/View controllers & view models/Track details/TrackDetailsViewController.swift
@@ -1,9 +1,16 @@
+import Alamofire
 import AVKit
 import UIKit
 
 class TrackDetailsViewController: UIViewController {
+    private enum Constant {
+        static let placeholderImageName = "track-placeholder"
+        static let transitionDuration: TimeInterval = 0.2
+    }
+
     @IBOutlet private var containerView: UIView!
     @IBOutlet private var spinner: UIActivityIndicatorView!
+    @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var artistNameLabel: UILabel!
     @IBOutlet private var trackNameLabel: UILabel!
     @IBOutlet private var genreLabel: UILabel!
@@ -14,25 +21,7 @@ class TrackDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        containerView.layer.borderColor = UIColor.blue.cgColor
-        containerView.layer.borderWidth = 1
         refreshView()
-    }
-
-    func refreshView() {
-        guard let viewModel = viewModel else {
-            fatalError("This screen's view model should be provided by the previous screen")
-        }
-        title = viewModel.trackName
-        artistNameLabel.text = viewModel.artistName
-        trackNameLabel.text = viewModel.trackName
-        genreLabel.text = viewModel.trackGenre
-        countryLabel.text = viewModel.trackCountry
-        if let releaseDate = viewModel.releaseDate {
-            releaseDateLabel.text = releaseDate
-        } else {
-            releaseDateLabel.isHidden = true
-        }
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender _: Any?) {
@@ -42,6 +31,40 @@ class TrackDetailsViewController: UIViewController {
         }
         pvc.viewModel = TrackPlayerViewModel(url: previewUrl)
         pvc.playerDelegate = self
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        let isLandscape = size.width > size.height
+        navigationController?.navigationBar.isHidden = isLandscape
+        view.backgroundColor = isLandscape ? .black : .systemBackground
+    }
+
+    private func refreshView() {
+        guard let viewModel = viewModel else {
+            fatalError("This screen's view model should be provided by the previous screen")
+        }
+        containerView.layer.cornerRadius = 5.0
+        title = viewModel.trackName
+        if let url = viewModel.artworkUrl {
+            imageView.af_setImage(
+                withURL: url,
+                placeholderImage: UIImage(named: Constant.placeholderImageName),
+                imageTransition: .crossDissolve(Constant.transitionDuration),
+                runImageTransitionIfCached: true
+            )
+        } else {
+            imageView.image = UIImage(named: Constant.placeholderImageName)
+        }
+        artistNameLabel.text = viewModel.artistName
+        trackNameLabel.text = viewModel.trackName
+        genreLabel.text = "Genre: " + viewModel.trackGenre
+        countryLabel.text = "Country: " + viewModel.trackCountry
+        if let releaseDate = viewModel.releaseDate {
+            releaseDateLabel.text = "Release date: " + releaseDate
+        } else {
+            releaseDateLabel.isHidden = true
+        }
     }
 }
 
