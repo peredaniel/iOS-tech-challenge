28689bef634b984a06305105b38f6a0e148ff143
Pere Daniel Prieto <math.pedro.daniel.prieto@gmail.com>
2019-10-12 20:13:59 +0200

    Added test to check conformance to protocol for SearchResultCollectionViewModel when model object is not of the expected type.

--- ABA MusicTests/Source/View models tests/SearchResultCollectionViewModelTests.swift
+++ ABA MusicTests/Source/View models tests/SearchResultCollectionViewModelTests.swift
@@ -16,7 +16,7 @@ class SearchResultCollectionViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.releaseDate, expectedReleaseDate)
     }
 
-    func testCellDataControllerConformance() {
+    func testCellDataControllerConformanceWithValidModel() {
         let track = Artist.generateArtist(index: 0).tracks.first!
         let viewModel = SearchResultCollectionViewModel.populate(with: track) as? SearchResultCollectionViewModelType
 
@@ -27,6 +27,19 @@ class SearchResultCollectionViewModelTests: XCTestCase {
         XCTAssertNotNil(viewModel)
         XCTAssertNotNil(viewModel?.artworkUrl)
         XCTAssertEqual(viewModel?.artworkUrl, expectedArtworkUrl)
+        XCTAssertEqual(viewModel?.name, expectedName)
+        XCTAssertEqual(viewModel?.releaseDate, expectedReleaseDate)
+    }
+
+    func testCellDataControllerConformanceWithNonValidModel() {
+        let model: Int = 0
+        let viewModel = SearchResultCollectionViewModel.populate(with: model) as? SearchResultCollectionViewModelType
+
+        let expectedName = ""
+        let expectedReleaseDate = "Jan 01, 1970"
+
+        XCTAssertNotNil(viewModel)
+        XCTAssertNil(viewModel?.artworkUrl)
         XCTAssertEqual(viewModel?.name, expectedName)
         XCTAssertEqual(viewModel?.releaseDate, expectedReleaseDate)
     }
